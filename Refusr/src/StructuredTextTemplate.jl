db626b473b5c6d00b5a77cbcfdcea2b34d72c211
Lucca Fraser <lucca.fraser@gmail.com>
2021-03-08 14:05:54 -0400

    some changes to the template

--- Refusr/src/StructuredTextTemplate.jl
+++ Refusr/src/StructuredTextTemplate.jl
@@ -3,31 +3,35 @@ module StructuredTextTemplate
 PREFIX = raw"""
 FUNCTION_BLOCK F_CollectInput
   VAR_IN_OUT
-    Data : ARRAY[1..50] OF BOOL;
+      Data : ARRAY[1..50] OF BOOL;
   END_VAR
   VAR_INPUT
-    TICK : BOOL;
-    IN1  : BOOL;
-    IN2  : BOOL;
-    IN3  : BOOL;
-    IN4  : BOOL;
-    IN5  : BOOL;
+      TICK  : BOOL := 0;
+      IN1   : BOOL := 0;
+      IN2   : BOOL := 0;
+      IN3   : BOOL := 0;
+      IN4   : BOOL := 0;
+      IN5   : BOOL := 0;
+      RESET : BOOL := FALSE;
   END_VAR
   VAR_OUTPUT
-    Finished : BOOL;
+      Finished : BOOL;
   END_VAR
   VAR
-    j    : USINT := 1;
-    tock : BOOL  := 0;
+      j    : USINT := 1;
+      tock : BOOL  := 0;
   END_VAR
-  IF tock = NOT TICK THEN
-    Data[j]   := IN1;
-    Data[j+1] := IN2;
-    Data[j+2] := IN3;
-    Data[j+3] := IN4;
-    Data[j+4] := IN5;
-    j := j + 5;
-    tock := TICK;
+  IF NOT RESET AND tock = NOT TICK THEN
+      Data[j]   := IN1;
+      Data[j+1] := IN2;
+      Data[j+2] := IN3;
+      Data[j+3] := IN4;
+      Data[j+4] := IN5;
+      j := j + 5;
+      tock := TICK;
+  ELSE
+      j := 1;
+      tock := 0;
   END_IF;
   Finished := (j > 50);
 END_FUNCTION_BLOCK
@@ -40,14 +44,14 @@ PROGRAM Boiler
     CollectInput : F_CollectInput;
   END_VAR
   VAR
-    TICK     AT %IX0.0 : BOOL;
-    IN1      AT %IX0.1 : BOOL;
-    IN2      AT %IX0.2 : BOOL;
-    IN3      AT %IX0.3 : BOOL;
-    IN4      AT %IX0.4 : BOOL;
-    IN5      AT %IX0.5 : BOOL;
-    OutReady AT %QX0.0 : BOOL := 0;
-    FeedNext AT %QX0.1 : BOOL := 0;
+    TICK     AT %IX1.0 : BOOL;
+    IN1      AT %IX0.3 : BOOL;
+    IN2      AT %IX0.4 : BOOL;
+    IN3      AT %IX0.5 : BOOL;
+    IN4      AT %IX0.6 : BOOL;
+    IN5      AT %IX0.7 : BOOL;
+    OutReady AT %QX0.0 : BOOL := FALSE;
+    FeedNext AT %QX0.1 : BOOL := FALSE;
     Out      AT %QX0.2 : BOOL;
   END_VAR
   CollectInput(TICK:=TICK, IN1:=IN1, IN2:=IN2, IN3:=IN3, IN4:=IN4, IN5:=IN5);
@@ -58,6 +62,7 @@ PROGRAM Boiler
 
 SUFFIX = raw"""
     OutReady := 1;
+    CollectInput(RESET:=TRUE);
   END_IF;
 END_PROGRAM
 
