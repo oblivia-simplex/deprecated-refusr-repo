2ee2f18aa562edde4e97230e8cc67dbce3b9accd
Lucca Fraser <lucca.fraser@gmail.com>
2021-07-07 22:14:47 -0300

    Implemented implicit fitness sharing, improved performance by orders of magnitude

--- Refusr/src/base.jl
+++ Refusr/src/base.jl
@@ -3,10 +3,10 @@ using Cockatrice
 using InformationMeasures
 using Statistics
 
-include("Names.jl")
 include("StructuredTextTemplate.jl")
-include("FF.jl")
 include("Expressions.jl")
+include("Names.jl")
+include("FF.jl")
 include("TreeGenotype.jl")
 include("LinearGenotype.jl")
 include("step.jl")
@@ -18,7 +18,17 @@ function get_likeness(g)
     isnothing(g.likeness) ? -Inf : maximum(g.likeness)
 end
 
+
+function objective_performance(g)
+    if g.phenotype === nothing
+        return -Inf
+    end
+    correct = (!).(g.phenotype.results .⊻ FF.get_answers(FF.DATA))
+    mean(correct)
+end
+
 TRACERS = [
+    (key="objective", callback=objective_performance, rate=1.0),
     (key="fitness_1", callback=g->g.fitness[1], rate=1.0),
     (key="fitness_2", callback=g->g.fitness[2], rate=1.0),
     (key="fitness_3", callback=g->g.fitness[3], rate=1.0),
@@ -32,24 +42,26 @@ TRACERS = [
 
 
 LOGGERS = [
+    (key="objective", reducer=maximum),
+    (key="objective", reducer=meanfinite),
+    (key="fitness_1", reducer=maximum),
     (key="fitness_1", reducer=meanfinite),
     (key="fitness_1", reducer=std),
-    (key="fitness_1", reducer=maximum),
+    (key="fitness_2", reducer=maximum),
     (key="fitness_2", reducer=meanfinite),
     (key="fitness_2", reducer=std),
-    (key="fitness_2", reducer=maximum),
-    (key="fitness_3", reducer=meanfinite),
-    (key="fitness_3", reducer=std),
-    (key="fitness_3", reducer=maximum),
-    (key="chromosome_len", reducer=Statistics.mean),
-    (key="chromosome_len", reducer=Statistics.std),
     (key="chromosome_len", reducer=Statistics.maximum),
-    (key="effective_len", reducer=Statistics.mean),
-    (key="effective_len", reducer=Statistics.std),
+    (key="chromosome_len", reducer=Statistics.mean),
     (key="effective_len", reducer=Statistics.maximum),
+    (key="effective_len", reducer=Statistics.mean),
     (key="num_offspring", reducer=maximum),
     (key="num_offspring", reducer=Statistics.mean),
     (key="generation", reducer=Statistics.mean),
     (key="likeness", reducer=meanfinite),
 ]
+
+
+## To facilitate debugging
+mkevo() = Cockatrice.Evo.Evolution(Cockatrice.Config.parse("./config.yaml"), creature_type=LinearGenotype.Creature, fitness=FF.fit, tracers=TRACERS, mutate=LinearGenotype.mutate!, crossover=LinearGenotype.crossover)
+
 
