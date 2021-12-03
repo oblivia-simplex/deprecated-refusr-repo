165c6b3467677285f98dd3214561460dc40e3ef4
Lucca Fraser <lucca.fraser@gmail.com>
2021-12-03 18:53:39 -0400

    directory reorg

---
+++ GP/refusr/src/Fingerprint.jl
@@ -0,0 +1,8 @@
+module Fingerprint
+
+# This module is a wrapper for the Fingerprint measure used in Anthony's Junta.jl
+
+using Junta
+
+
+end
--- scripts/generate_mux.jl
+++
@@ -1,6 +0,0 @@
-SCRIPT_DIR = dirname(@__FILE__)
-SRC = "$(SCRIPT_DIR)/../src/"
-
-include(SRC * "Generate.jl")
-
-ARGS
